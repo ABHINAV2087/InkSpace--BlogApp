8a133e6fc9a05c166d6d1774957abdb591895af3
Abhinav Tirole <abhinav.tirole2021@vitbhopal.ac.in>
2024-09-15 23:47:57 +0530

    first commit

--- blogBackend/index.js
+++ blogBackend/index.js
@@ -17,7 +17,7 @@ const cookieParser = require('cookie-parser');
 
 
 app.use(bodyParser.json());
-const allowedOrigins = ['http://localhost:5173','https://inkspace-blogapp-frontend.onrender.com']; // Add more origins as needed
+const allowedOrigins = ['http://localhost:5173','https://inkspace-blogapp.onrender.com/']; // Add more origins as needed
 
 // Configure CORS with credentials
 app.use(
